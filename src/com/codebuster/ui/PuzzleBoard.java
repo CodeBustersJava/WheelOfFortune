6e632d9c816a8853a7b8e3b032f4ab479e67f68c
dustinmorris1 <dustinnuc@gmail.com>
2020-11-07 20:23:28 -0800

    Changed puzzleBoardModel to a boolean for efficiency. initial code of storeTempLetter UI letters and numbers stored as a text file

--- src/com/codebuster/ui/PuzzleBoard.java
+++ src/com/codebuster/ui/PuzzleBoard.java
@@ -1,53 +1,72 @@
 package com.codebuster.ui;
 
 import com.codebuster.enums.Category;
-
-import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class PuzzleBoard {
-    private String[][][] puzzleBoard;
-    private char[] puzzleBoardModel;
+    private String[][] puzzleBoard;
+    private boolean[] puzzleBoardModel;
     private String category;
     private String puzzle;
     private final int letterHeight = 5;
     private final int letterWidth = 10;
 
-    PuzzleBoard(Category category, String puzzle){
+    PuzzleBoard(Category category, String puzzle) {
         setCategory(category);
         setPuzzle(puzzle);
-        puzzleBoard = new String[puzzle.length()][letterHeight][letterWidth];
-        puzzleBoardModel = new char[puzzle.length()];
+        puzzleBoard = new String[puzzle.length()][letterHeight];
+        puzzleBoardModel = new boolean[puzzle.length()];
     }
 
-    private void buildDisplay(){
-        String[][] tempLetter = null;
-        for(int letter = 0; letter < puzzle.length(); letter++){
-            if(puzzle.charAt(letter) != '_'){
-                if(puzzleBoardModel[letter] == '\000'){
-                    if(tempLetter == null){
+    private void buildDisplay() {
+        String[] tempLetter = null;
+        for (int letter = 0; letter < puzzle.length(); letter++) {
+            if (puzzle.charAt(letter) != ' ') {
+                if (puzzleBoardModel[letter] == false) {
+                    if (tempLetter == null) {
                         tempLetter = storeTempLetter(puzzle.charAt(letter));
-                        puzzleBoardModel[letter] = puzzle.charAt(letter);
+                        puzzleBoardModel[letter] = true;
                     }
                     puzzleBoard[letter] = tempLetter;
                 }
-            }else if(puzzle.charAt(letter) == ' '){
-                for(int row = 0; row < letterHeight; row++){
-                    for(int col = 0; col < letterWidth; col++){
-                        puzzleBoard[letter][row][col] = " ";
-                    }
+            } else {
+                for (int row = 0; row < letterHeight; row++) {
+                        puzzleBoard[letter][row] = "       ";
                 }
             }
         }
-        // loop through each letter in the array
-        // if the letter is not an "_" underscore then look for it in the puzzleBoard
-        // if it isn't already in the puzzleBoard
-        //  then look for a temp variable
-        //  if not load it from a file into a temp variable
-        // add temp variable to the corresponding place in the puzzleBoard
     }
 
-    private String[][] storeTempLetter(char letter){
-        return new String[letterHeight][letterWidth];
+    private String[] storeTempLetter(char letter) {
+        String[] displayLetter = new String[letterHeight];
+        String letterString;
+        if(letter == '_'){
+            letterString = "BLANK";
+        }else{
+            letterString = String.valueOf(letter).toUpperCase();
+        }
+        String directory;
+        try{
+            directory = System.getProperty("user.dir");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+            return new String[0];
+        }
+        File file = new File(directory + File.pathSeparator
+                + "Letters" + File.pathSeparator + letterString + ".txt");
+        Scanner sc;
+        try {
+            sc = new Scanner(file);
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+            return new String[0];
+        }
+        for(int i = 0; i < letterHeight; i++){
+            displayLetter[i] = sc.nextLine();
+        }
+        return displayLetter;
     }
 
     public void setCategory(Category category) {
@@ -59,7 +78,7 @@ public class PuzzleBoard {
         buildDisplay();
     }
 
-    public String[][][] getPuzzleBoard() {
+    public String[][] getPuzzleBoard() {
         return puzzleBoard;
     }
 }
