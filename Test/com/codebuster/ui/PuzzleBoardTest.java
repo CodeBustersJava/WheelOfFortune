e6e12fcd8b11a7332bc434a67121d378ef33b079
dustinmorris1 <dustinnuc@gmail.com>
2020-11-12 09:33:01 -0800

    moved WheelOfFortune to client

--- Test/com/codebuster/ui/PuzzleBoardTest.java
+++ Test/com/codebuster/ui/PuzzleBoardTest.java
@@ -5,30 +5,33 @@ import org.junit.Test;
 public class PuzzleBoardTest {
     @Test
     public void createPuzzleBoardTest(){
-//        PuzzleBoard puzzle = new PuzzleBoard("Author", "This is my puzzle");
-//        String[][] puzzleArray = puzzle.getPuzzleBoard();
-//        for(int j = 0; j < puzzleArray[1].length; j++) {
-//            System.out.println();
-//            for (String[] lines : puzzleArray) {
-//                System.out.print(lines[j]);
-//            }
-//        }
-//        PuzzleBoard puzzle2 = new PuzzleBoard("Author", "____ M_ __ __ZZ__");
-//        String[][] puzzleArray2 = puzzle2.getPuzzleBoard();
-//        for(int j = 0; j < puzzleArray2[3].length; j++) {
-//            System.out.println();
-//            for (String[] lines : puzzleArray2) {
-//                System.out.print(lines[j]);
-//            }
-//        }
-        PuzzleBoard puzzle3 = new PuzzleBoard("Author", "_____EE_");
-        String[][] puzzleArray3 = puzzle3.getPuzzleBoard();
-        for(int j = 0; j < 3; j++) {
-            for (String[] lines : puzzleArray3) {
+        PuzzleBoard puzzle = new PuzzleBoard("Author", "This is my puzzle");
+        String[][] puzzleArray = puzzle.getPuzzleBoard();
+        for(int j = 0; j < puzzleArray[1].length; j++) {
+            System.out.println();
+            for (String[] lines : puzzleArray) {
                 System.out.print(lines[j]);
             }
             System.out.println();
         }
+        PuzzleBoard puzzle2 = new PuzzleBoard("Author", "____ M_ __ __ZZ__");
+        String[][] puzzleArray2 = puzzle2.getPuzzleBoard();
+        for(int j = 0; j < puzzleArray2[3].length; j++) {
+            System.out.println();
+            for (String[] lines : puzzleArray2) {
+                System.out.print(lines[j]);
+            }
+        }
+        System.out.println();
+        PuzzleBoard puzzle3 = new PuzzleBoard("Author", "_____EE_");
+        String[][] puzzleArray3 = puzzle3.getPuzzleBoard();
+        for(int j = 0; j < 3; j++) {
+            System.out.println();
+            for (String[] lines : puzzleArray3) {
+                System.out.print(lines[j]);
+            }
+        }
+        System.out.println();
         puzzle3.updatePuzzle("Bonnie");
         String[][] puzzleArray4 = puzzle3.getPuzzleBoard();
         for(int j = 0; j < puzzleArray4[3].length; j++) {
@@ -37,11 +40,12 @@ public class PuzzleBoardTest {
                 System.out.print(lines[j]);
             }
         }
+        System.out.println();
     }
 
 //    @Test
 //    public void storeLetterTest(){
-//        PuzzleBoard puzzle = new PuzzleBoard(Category.AROUND_THE_HOUSE,"Test_This");
+//        PuzzleBoard puzzle = new PuzzleBoard("AROUND_THE_HOUSE","Test_This");
 //        String[] letter = puzzle.storeLetter('A');
 //        for(String line : letter){
 //            System.out.println(line);
